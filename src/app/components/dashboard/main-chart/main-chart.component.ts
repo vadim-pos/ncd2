4405c4f57945ece02e56da0c69089117ce80a226
Vadzim Pastanoukin <vadzim.pastanoukin@itechart-group.com>
2017-08-28 10:00:53 +0300

    cleanup & update styles

--- src/app/components/dashboard/main-chart/main-chart.component.ts
+++ src/app/components/dashboard/main-chart/main-chart.component.ts
@@ -4,7 +4,7 @@ declare const d3;
 @Component({
   selector: 'app-main-chart',
   template: `
-    <div>
+    <div class="main-chart">
       <nvd3 [options]="options" [data]="data"></nvd3>
     </div>
   `,
@@ -54,53 +54,10 @@ export class MainChartComponent implements OnInit {
         },
         yAxis: {
           showMaxMin: true,
-        },
-        interactiveLayer: {
-          tooltip: {
-            // position: () => this.generateTooltipContent(),
-            hidden: () => true
-            // enabled: false
-          }
-        },
-        // callback: chart => chart.interactiveLayer.tooltip.contentGenerator(this.generateTooltipContent)
-        // callback: chart => chart.interactiveLayer.tooltip.contentGenerator(this.generateTooltipContent)
-        callback: chart => console.dir(chart.interactiveLayer.tooltip)
+        }
       }
     };
 
     this.data = this.chartData;
   }
-
-  generateTooltipContent() {
-    console.log(1111);
-    // return `
-    //   <div style="box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)">
-    //     <span>${ d3.time.format('%x')(new Date(data.value)) }</span>
-    //     <table>
-    //       ${data.series.forEach(serie => (
-    //         `<tr>
-    //           <td><span style="display: block;width: 12px;height: 12px; border: 1px solid #fff; background=${serie.color}"></span></td>
-    //         </tr>`
-    //       ))}
-    //     </table>
-    //   </div>
-    // `;
-
-
-    // `<div #balloon class="balloon" [class.visible]="balloonIsVisible" [ngStyle]="balloonStyles">
-    //   <div class="balloon-header">{{selectedCursorData ? selectedCursorData.date : ''}}</div>
-    //   <table class="balloon-table">
-    //     <tr class="balloon-item"
-    //         *ngFor="let type of availableTypesOrdered"
-    //         [class.selected]="selectedGraphItem && selectedGraphItem === type"
-    //         [style.background] = "selectedGraphItem === type ? planColorsMap[type] : ''"
-    //         >
-    //       <td class="balloon-cell marker-cell"><span class="balloon-marker" [style.background]="planColorsMap[type]"></span></td>
-    //       <td class="balloon-cell type-cell">{{chartPlanNamesMap[type]}}</td>
-    //       <td class="balloon-cell value-cell">{{selectedCursorData ? selectedCursorData[type] : ''}}</td>
-    //     </tr>
-    //   </table>
-    // </div>`
-  }
-
 }
